6a4b3d7ea30a9f018fc3bd846fb83b31b9c6c14b
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-11-28 18:14:54 +0200

    Color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+const bodyColor = document.querySelector('body')
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+let intervalId = null;
+  const setBackgroundColor = () => {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+    console.log(setBackgroundColor);
+  }
+  
+  const onStartBtnPress = () => {
+    intervalId = setInterval(() => {
+    setBackgroundColor()
+  },
+      1000)
+    
+    startBtn.disabled = true;
+  }
+
+  const onStopBtnPress = () => {
+   // console.log('stop')
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+  }
+startBtn.addEventListener('click', onStartBtnPress);
+stopBtn.addEventListener('click', onStopBtnPress);
+
+    
+
 
