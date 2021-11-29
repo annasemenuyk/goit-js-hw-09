02dadfb771637f1cb66b2e05ccfff56568c39172
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-11-29 23:23:08 +0200

    123

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,26 +1,31 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 const inputDate = document.getElementById('datetime-picker')
-const date = new Date;
-console.log(date)
-flatpickr(selector, options)
-// const options = {
-//     enableTime: true, //Включает сборщик времени
-//     time_24hr: true, //Отображает указатель времени в 24-часовом режиме без выбора AM / PM, если он включен
-//     defaultDate: new Date(), //Устанавливает начальную выбранную дату (даты)
-//     minuteIncrement: 1, //Регулирует шаг ввода минут (включая прокрутку)
-//     onClose(selectedDates) {
-//       console.log(selectedDates[0]);
-//     },
-//   };
-function convertMs(ms) {
+const date = new Date();
+console.log(date.getTime());
+ inputDate.addEventListener('input', selectedFutureDate,convertMs)
+ flatpickr(selector, options)
+ const options = {
+  enableTime: true, //Включает сборщик времени
+  time_24hr: true, //Отображает указатель времени в 24-часовом режиме без выбора AM / PM, если он включен
+  defaultDate: new Date(), //Устанавливает начальную выбранную дату (даты)
+  minuteIncrement: 1, //Регулирует шаг ввода минут (включая прокрутку)
+}
+function selectedFutureDate (){
+  let flatpickrOptions = {};
+  return flatpickrOptions;
+}
+  inputDate.addEventListener('input',onClose)
+  function onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  };
+ function convertMs(ms) {
     // Number of milliseconds per unit of time
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
-  
-    // Remaining days
+      // Remaining days
     const days = Math.floor(ms / day);
     // Remaining hours
     const hours = Math.floor((ms % day) / hour);
@@ -28,10 +33,5 @@ function convertMs(ms) {
     const minutes = Math.floor(((ms % day) % hour) / minute);
     // Remaining seconds
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
+      return { days, hours, minutes, seconds };
   }
-  
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
