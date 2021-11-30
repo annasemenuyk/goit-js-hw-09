430fe3e38a12c0221f4a2946148fae01d940411c
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-12-01 01:05:29 +0200

    1233

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,37 +1,59 @@
 import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-const inputDate = document.getElementById('datetime-picker')
-const date = new Date();
-console.log(date.getTime());
- inputDate.addEventListener('input', selectedFutureDate,convertMs)
- flatpickr(selector, options)
- const options = {
+import "flatpickr/dist/themes/dark.css";
+import Notiflix from 'notiflix';
+const selector = document.getElementById('datetime-picker')
+const startBtn = document.querySelector("[data-start]");
+startBtn.disabled = true;
+//console.log(date.getTime());
+flatpickr("#datetime-picker",{
   enableTime: true, //Включает сборщик времени
   time_24hr: true, //Отображает указатель времени в 24-часовом режиме без выбора AM / PM, если он включен
   defaultDate: new Date(), //Устанавливает начальную выбранную дату (даты)
   minuteIncrement: 1, //Регулирует шаг ввода минут (включая прокрутку)
-}
-function selectedFutureDate (){
-  let flatpickrOptions = {};
-  return flatpickrOptions;
-}
-  inputDate.addEventListener('input',onClose)
-  function onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  };
- function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-      // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-      return { days, hours, minutes, seconds };
+  onClose(selectedDates) {
+    const currentTime = Date.now()
+   if (selectedDates < currentTime){
+   return window.alert(`"Please choose a date in the future"`);
+   }
+   startBtn.disabled = false;
   }
+})
+
+  //     const targetDate = targetDate;
+  //     const selector = document.querySelector('.field');
+  //     const deltaTime = document.querySelector('.value');
+  //     const deltadays = document.querySelector('[data-days]');
+  //     const deltahours = document.querySelector('[data-hours]');
+  //     const deltaminutes = document.querySelector('[data-minutes]');
+  //     const deltaseconds = document.querySelector('[data-seconds]');
+  //     const deltastartBtn = document.querySelector('[data-start]');
+  //     convertMs(ms) {
+  //       // Number of milliseconds per unit of time
+  //       const second = 1000;
+  //       const minute = second * 60;
+  //       const hour = minute * 60;
+  //       const day = hour * 24;
+  //         // Remaining days
+  //       const days = pad(Math.floor(ms / day));
+  //       // Remaining hours
+  //       const hours = pad(Math.floor((ms % day) / hour));
+  //       // Remaining minutes
+  //       const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  //       // Remaining seconds
+  //       const seconds = pad( Math.floor((((ms % day) % hour) % minute) / second));
+  //         return ({ days, hours, minutes, seconds }).padStart(2, '0');
+  //     }
+  function start() {
+        setInterval(() => {
+            const currentTime = Date.now();
+            const deltaTime = this.targetDate - currentTime;
+            console.log('deltatime', deltaTime)
+
+            const time = convertMs(deltaTime);
+            selectedDates(ms);
+        },
+        1000)
+    }
+
+  
+    
