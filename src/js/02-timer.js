901989c1d3f4f638166bbb4b80b01f6a2e152ead
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-12-02 00:50:20 +0200

    3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,16 +2,6 @@ import Notiflix from 'notiflix';
 import flatpickr from "flatpickr";
 import "flatpickr/dist/themes/dark.css";
 
-flatpickr("#datetime-picker",{
-  enableTime: true, //Включает сборщик времени
-  time_24hr: true, //Отображает указатель времени в 24-часовом режиме без выбора AM / PM, если он включен
-  defaultDate: new Date(), //Устанавливает начальную выбранную дату (даты)
-  minuteIncrement: 1, //Регулирует шаг ввода минут (включая прокрутку)
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    dateSelected(selectedDates[0]);
-},
-});
 const selector = document.getElementById('datetime-picker');
 const startBtn = document.querySelector("[data-start]");
 // const fieldBlocK = document.querySelector('.field');
@@ -21,12 +11,28 @@ const deltaDays = document.querySelector('[data-days]');
 const deltaHours = document.querySelector('[data-hours]');
 const deltaMinutes = document.querySelector('[data-minutes]');
 const deltaSeconds = document.querySelector('[data-seconds]');
-  let intervalId = null;
+
+flatpickr("#datetime-picker",{
+  enableTime: true, //Включает сборщик времени
+  time_24hr: true, //Отображает указатель времени в 24-часовом режиме без выбора AM / PM, если он включен
+  defaultDate: new Date(), //Устанавливает начальную выбранную дату (даты)
+  minuteIncrement: 1, //Регулирует шаг ввода минут (включая прокрутку)
+  onClose(selectedDates) {
+  //  console.log(selectedDates[0]);
+    dateSelected(selectedDates[0]);
+},
+});
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  selector.disabled = true;
+ });
   let selectedDate = null;
-  const dateSelected = date => {
-  selectedDate = date;
+  let intervalId = null;
   const dateNow = new Date();
   //console.log(date.getTime());
+  const dateSelected = date => {
+    selectedDate = date;
+    const dateNow = new Date();
   if (date.getTime() < dateNow.getTime()) {
     startBtn.disabled = true;
     Notiflix.Notify.warning('Please choose a date in the future');
@@ -36,21 +42,24 @@ const deltaSeconds = document.querySelector('[data-seconds]');
   intervalId = setInterval(intervalDeltaTime, 1000);
   };
   const intervalDeltaTime = () => {
-  const dateNow = new Date();
+    const dateNow = new Date();
   if (dateNow.getTime() < selectedDate.getTime) {
   clearInterval(intervalId);
   }
-  startBtn.addEventListener('click', () => {
-    startBtn.disabled = true;
-    selector.disabled = true;
-     });
+  const deltaMs = selectedDate.getTime() - dateNow.getTime();
+  const { days, hours, minutes, seconds } = convertMs(deltaMs);
+  addLeadingZero(days, deltaDays);
+  addLeadingZero(hours, deltaHours);
+  addLeadingZero(minutes,deltaMinutes);
+  addLeadingZero(seconds, deltaSeconds);
+   };
+
   function convertMs(ms) {
     // Number of milliseconds per unit of time
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
-  
     // Remaining days
     const days = Math.floor(ms / day);
     // Remaining hours
@@ -59,22 +68,18 @@ const deltaSeconds = document.querySelector('[data-seconds]');
     const minutes = Math.floor(((ms % day) % hour) / minute);
     // Remaining seconds
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
     return { days, hours, minutes, seconds };
+    
   }
   // console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
   // console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
   // console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-  const deltaMs = selectedDate.getTime() - dateNow.getTime();
-  const { days, hours, minutes, seconds } = convertMs(deltaMs);
-  addLeadingZero(days, deltaDays);
-  addLeadingZero(hours, deltaHours);
-  addLeadingZero(minutes,deltaMinutes);
-  addLeadingZero(seconds, deltaSeconds);
-  };
+
+
   function addLeadingZero(value, element) {
-  element.textContent = String(value).padStart(2, '0');
+  element.textContent = String(value).padStart(2, '0')
   }
+
 
 document.body.style.textAlign ='centr';
 document.body.style.boxShadow = '0 0 15px #212121';
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import Notiflix from 'notiflix';
+const formPromise = document.querySelector('.form');
+formPromise.addEventListener("submit", onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault(event);
+  const delay = Number(document.querySelector('input[name="delay"]').value);
+  const step = Number(document.querySelector('input[name="step"]').value);
+  const amount  = Number(document.querySelector('input[name="amount"]').value);
+  
+  let currentDelay = delay;
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise( i,currentDelay)
+    onFormSubmit.then(({ position, delay }) => {
+     // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    onFormSubmit.catch(({ position, delay }) => {
+     // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+    currentDelay += step;
+  }
+} 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+  setTimeout(() => {
   if (shouldResolve) {
-    // Fulfill
+    resolve({ position, delay });// Fulfill
   } else {
-    // Reject
+    reject({ position, delay });// Reject
   }
-}
+},2000)
+return createPromise
+};
